c9621701acb396f790233a0dac620599aa78bd22
Edson Júnior <ebf.junin@gmail.com>
2018-05-04 22:35:00 -0300

    Added Buyer resolver on graphql server

--- src/lib/graphql/schema.py
+++ src/lib/graphql/schema.py
@@ -1,11 +1,13 @@
 import graphene
 from lib.graphql.product import Product, resolve_Products
 from lib.graphql.seller import Seller, resolve_Sellers
+from lib.graphql.buyer import Buyer, resolve_Buyers
 from lib.graphql.cart import Cart, resolve_Cart
 
 class Query(graphene.ObjectType):
   Sellers = graphene.Field(graphene.List(lambda: Seller), name=graphene.String())
   Products = graphene.Field(graphene.List(lambda: Product), name=graphene.String(), category=graphene.String(), sellerIdentifier=graphene.String())
+  Buyers = graphene.Field(graphene.List(lambda: Buyer))
   Cart = graphene.Field(Cart, id=graphene.String())
 
   def resolve_Sellers(self, info, name):
@@ -16,5 +18,8 @@ class Query(graphene.ObjectType):
 
   def resolve_Cart(self, info, id):
     return resolve_Cart(info, id)
+
+  def resolve_Buyers(self, info):
+    return resolve_Buyers()
 
 schema = graphene.Schema(query=Query)
